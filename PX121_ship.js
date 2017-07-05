c8e469a46bca0de951496c8046cde8b2aebd7b5e
MrEranwe <ivanez92@hotmail.com>
2017-07-05 15:47:30 +0200

    First version

---
+++ PX121_ship.js
@@ -0,0 +1,18 @@
+var ship = {
+    VesselData: {
+        Id: 1,
+        Name: "Ulstein PX121",
+        LOA: 82,
+        BOA: 18,
+        D: 6.5,
+        BlockCoefficient: 0.75,
+        PrismaticCoefficient: 0.8,
+        MaxSpeed: 30,
+        Bulkheads: ["AB"],
+	    BHPos: [50],
+    },
+	HullData: [10, 10, 10, 2.2, 0.99, 0.07, 0.28, 0.99, 0],
+	Methods:{
+        
+    }
+};
